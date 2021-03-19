a32d958712ccb40a1e49f31a9f9773fea7769046
sunshuofeng <45551402+sunshuofeng@users.noreply.github.com>
2021-03-19 20:08:41 +0800

    Update nncf_train.py

--- nncf_train.py
+++ nncf_train.py
@@ -301,7 +301,7 @@ def train(hyp, opt, device, tb_writer=None, wandb=None):
             pbar = tqdm(pbar, total=nb)  # progress bar
         optimizer.zero_grad()
 
-        compress_ctrl.scheuler.epoch_step()
+        compress_ctrl.scheduler.epoch_step()
         for i, (imgs, targets, paths, _) in pbar:  # batch -------------------------------------------------------------
             ni = i + nb * epoch  # number integrated batches (since train start)
             imgs = imgs.to(device, non_blocking=True).float() / 255.0  # uint8 to float32, 0-255 to 0.0-1.0
@@ -327,7 +327,7 @@ def train(hyp, opt, device, tb_writer=None, wandb=None):
 
 
             print(imgs.shape)
-            compress_ctrl.scheuler.step()
+            compress_ctrl.scheduler.step()
             # Forward
             with amp.autocast(enabled=cuda):
                 pred = model(imgs)  # forward
